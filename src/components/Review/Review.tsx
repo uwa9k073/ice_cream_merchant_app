0be7bc9d8d3c32b597c56f36ce4c55d500220918
Константин Орлов <konsor-79@mail.ru>
2023-10-29 18:28:07 +0300

    feat product: update review
    Add (dis-) like  buttons to review component
    
    Relates: #6

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -1,4 +1,4 @@
-import { Text } from "@adev/ui-kit";
+import { Button, Text } from "@adev/ui-kit";
 import { IReview } from ".";
 import styles from "./Review.module.scss"
 
@@ -12,6 +12,18 @@ const Review = ({ review }: IReviewProps) => {
             <div className={styles.wrapper}>
                 <Text typography="headline-md">{review.userName}</Text>
                 <Text typography="body-long-sm">{review.text}</Text>
+                <div>
+                    <Button size="xs">
+                        <span className="material-symbols-outlined">
+                            thumb_up
+                        </span>
+                    </Button>
+                    <Button size="xs">
+                        <span className="material-symbols-outlined">
+                            thumb_down
+                        </span>
+                    </Button>
+                </div>
             </div>
         </>
     )
